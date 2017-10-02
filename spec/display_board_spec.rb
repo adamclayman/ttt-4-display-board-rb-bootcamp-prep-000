ca8ba0e90ca4871cf4758565f365d47c88b1b940
Adam Clayman <adam@clayman.org>
2017-10-02 16:29:04 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -147,9 +147,6 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(rows[2]).to eq(" X | X | X ")
       expect(rows[3]).to eq("-----------")
       expect(rows[4]).to eq(" X | X | X ")
-
-      # *** Comment the line below by adding a # at the line start ***
-      # expect(true).to be(true)
     end
 
     it 'prints an entire board full of Os' do
@@ -163,7 +160,6 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(rows[2]).to eq(" O | O | O ")
       expect(rows[3]).to eq("-----------")
       expect(rows[4]).to eq(" O | O | O ")
-      expect(true).to be(true)
     end
   end
 end
